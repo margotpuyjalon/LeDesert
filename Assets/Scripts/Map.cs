20e09d9505d12d5e25440db2c8212d3103f62b6d
MargotW <m.puyjalon@gmail.com>
2019-04-02 15:03:20 +0200

    Generate the map

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -21,22 +21,31 @@ enum Type
 
 public class Map : MonoBehaviour
 {
-    //Penser à faire une liste des tuiles du plateau
-    //pour les intégrer dans la map lors de la génération
+    const int NB_TILE = 25;
 
-    const int NB_SOUCE = 1;
-    const int NB_TUNNEL = 2;
-
-    public GameObject Tile_tech;
+    public GameObject Tile_tech, Tile_source, Tile_start, Tile_end, Tile_tunnel;
 
     private Tile[,] map;
+    private Tile[] mendatoryTiles;
     private int difficulty;
     private int sandBlocksLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartGame(25);
+        mendatoryTiles = new Tile[]
+            {
+                new Tile((int)Type.SOURCE),
+                new Tile((int)Type.TUNNEL), new Tile((int)Type.TUNNEL),
+                new Tile((int)Type.START),
+                new Tile((int)Type.END),
+                new Tile((int)Type.HP1), new Tile((int)Type.VP1),
+                new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH),
+                new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH),
+                new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH),
+                new Tile((int)Type.TECH), new Tile((int)Type.TECH), new Tile((int)Type.TECH)
+            };
+        StartGame(5, 5);
     }
 
     // Update is called once per frame
@@ -48,21 +57,36 @@ public class Map : MonoBehaviour
     // Crée une nouvelle matrice de tuiles
     // Place le joueur sur la tuile de départ
     // Affiche les éléments du jeu
-    void StartGame(int nbTiles)
+    void StartGame(int x, int y)
     {
-        GenerateMatrix(5, 5);
-        DisplayMap(5, 5);
+        ShuffleTiles();
+        GenerateMatrix(x, y);
+        DisplayMap(x, y);
     }
 
-    // Fil map with Tiles
+    // Shuffle the mendatoryTiles tab
+    void ShuffleTiles()
+    {
+        for (int i = 0; i < mendatoryTiles.Length; i++)
+        {
+            Tile temp = mendatoryTiles[i];
+            int randomIndex = Random.Range(i, mendatoryTiles.Length);
+            mendatoryTiles[i] = mendatoryTiles[randomIndex];
+            mendatoryTiles[randomIndex] = temp;
+        }
+    }
+
+    // Fill map with Tiles
     void GenerateMatrix(int x, int y)
     {
-        map = new Tile[x,y];
+        int k = 0;
+        map = new Tile[x, y];
         for(int i=0; i<x; i++)
         {
             for(int j=0; j<y; j++)
             {
-                map[i, j] = new Tile((int)Type.TECH);
+                map[i, j] = mendatoryTiles[k];
+                k++;
             }
         }
     }
@@ -74,12 +98,35 @@ public class Map : MonoBehaviour
         {
             for (int j = 0; j < y; j++)
             {
-                GameObject tileTech = Instantiate(Tile_tech);
-                tileTech.transform.position = new Vector2(-1.5f + i, 2.5f - j);
+                GameObject tileType = null;
+                switch ((Type)map[i,j].type)
+                {
+                    case Type.SOURCE:
+                        tileType = Tile_source;
+                        break;
+                    case Type.TECH:
+                        tileType = Tile_tech;
+                        break;
+                    case Type.START:
+                        tileType = Tile_start;
+                        break;
+                    case Type.END:
+                        tileType = Tile_end;
+                        break;
+                    case Type.TUNNEL:
+                        tileType = Tile_tunnel;
+                        break;
+                    default:
+                        tileType = Tile_tech;
+                        break;
+                }
+                GameObject tile = Instantiate(tileType);
+                tile.transform.position = new Vector3(-1.5f + i, 2.5f - j, 1);
             }
         }
     }
 
+    // FOR DEBUG
     //void Display(int x, int y)
     //{
     //    print("map");
