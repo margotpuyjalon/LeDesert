c62aaff65c4bce51be6efe8e3228ed6d1353c9df
nicolas-raymond <37674067+nicolas-raymond@users.noreply.github.com>
2019-04-02 11:55:54 +0200

    Mise en place du Deck

---
+++ Assets/Scripts/DeckManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<int> deck = new List<int>();
+    public int indexToPick = 0;
+
+    enum CardsType {
+        HeatWave,
+        DifficultyUp,
+        MoveOneToLeft,
+        MoveOneToRight,
+        MoveOneToTop,
+        MoveOneToBot,
+        MoveTwoToLeft,
+        MoveTwoToRight,
+        MoveTwoToTop,
+        MoveTwoToBot,
+        MoveThreeToLeft,
+        MoveThreeToRight,
+        MoveThreeToTop,
+        MoveThreeToBot,
+    };
+    //
+    int nbHeatWave = 2; 
+    int nbDifficultyUp = 2; 
+    //
+    int nbMoveOneToLeft = 2; 
+    int nbMoveOneToRight = 2; 
+    int nbMoveOneToTop = 2; 
+    int nbMoveOneToBot = 2;
+    //
+    int nbMoveTwoToLeft = 2; 
+    int nbMoveTwoToRight = 2; 
+    int nbMoveTwoToTop = 2; 
+    int nbMoveTwoToBot = 2;
+    //
+    int nbMoveThreeToLeft = 2;
+    int nbMoveThreeToRight = 2;
+    int nbMoveThreeToTop = 2;
+    int nbMoveThreeToBot = 2; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateDeck();
+        Shuffle();
+    }
+
+    void CreateDeck()
+    {
+        for (int i = 0; i < nbHeatWave; i++){ deck.Add((int)CardsType.HeatWave); }
+        for (int i = 0; i < nbDifficultyUp; i++) { deck.Add((int)CardsType.DifficultyUp); }
+        for (int i = 0; i < nbMoveOneToLeft; i++) { deck.Add((int)CardsType.MoveOneToLeft); }
+        for (int i = 0; i < nbMoveOneToRight; i++) { deck.Add((int)CardsType.MoveOneToRight); }
+        for (int i = 0; i < nbMoveOneToTop; i++) { deck.Add((int)CardsType.MoveOneToTop); }
+        for (int i = 0; i < nbMoveOneToBot; i++) { deck.Add((int)CardsType.MoveOneToBot); }
+        for (int i = 0; i < nbMoveTwoToLeft; i++) { deck.Add((int)CardsType.MoveTwoToLeft); }
+        for (int i = 0; i < nbMoveTwoToRight; i++) { deck.Add((int)CardsType.MoveTwoToRight); }
+        for (int i = 0; i < nbMoveTwoToTop; i++) { deck.Add((int)CardsType.MoveTwoToTop); }
+        for (int i = 0; i < nbMoveTwoToBot; i++) { deck.Add((int)CardsType.MoveTwoToBot); }
+        for (int i = 0; i < nbMoveThreeToLeft; i++) { deck.Add((int)CardsType.MoveThreeToLeft); }
+        for (int i = 0; i < nbMoveThreeToRight; i++) { deck.Add((int)CardsType.MoveThreeToRight); }
+        for (int i = 0; i < nbMoveThreeToTop; i++) { deck.Add((int)CardsType.MoveThreeToTop); }
+        for (int i = 0; i < nbMoveThreeToBot; i++) { deck.Add((int)CardsType.MoveThreeToBot); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shuffle() {
+        // mélange de Fisher-Yates ou de Knuth
+        for (int i = 0; i < deck.Count; i++)
+        {
+            int temp = deck[i];
+            int randomIndex = Random.Range(i, deck.Count);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
+
+    public int PickNextCard()
+    {
+        int cardToReturn = deck[indexToPick];
+        // Incrémentation pour la prochaine pioche
+        if (indexToPick == deck.Count - 1)
+        {
+            indexToPick = 0;
+
+        }
+        else { indexToPick++; }
+
+        return cardToReturn;
+    }
+}
